16a8eaed5d90493efe87d52c79c775e31c009c50
Tom Ridge <tom.j.ridge@googlemail.com>
2018-04-16 17:04:09 +0100

    add doc to mli

--- tjr_path_resolution.mli
+++ tjr_path_resolution.mli
@@ -17,9 +17,11 @@ type ('file_id,'dir_id,'t) fs_ops = {
 }
 
 
-(** The state maintained during path resolution; any returned state
-    should have is_absolute=false, so this is only useful for cwd and
-    path *)
+(** The state maintained during path resolution; any user-visible (ie
+    returned to user from this library) state should have
+    is_absolute=false, so the only user-visible components that are
+    useful are cwd and path *)
+(* FIXME could make abstract, with projection functions *)
 type 'dir_id state = {
   cwd: 'dir_id;
   is_absolute: bool;
@@ -30,7 +32,16 @@ type 'dir_id state = {
 (** The result of path resolution... *)
 type ('file_id,'dir_id) simplified_result' = File of 'file_id | Dir of 'dir_id | Sym of string | Missing
 
-(** ... *)
+(** 
+
+We have the following information about a resolved path:
+
+- [parent_id] is the directory which contains the result
+- [comp] the name of the result within the parent
+- [result] the simplified result (file,dir,sym or missing)
+- [trailing_slash] whether there was a **single** trailing slash left to process
+
+*)
 type ('file_id,'dir_id) simplified_result = 
     { parent_id: 'dir_id; comp: comp_; result: ('file_id,'dir_id) simplified_result'; trailing_slash:bool }
 
@@ -38,7 +49,7 @@ type ('file_id,'dir_id) simplified_result =
 (** The resolve function itself. 
 
 Errors:
-- File_followed_by_slash_etc: f.txt/some/more/stuff or f.txt// (not f.txt/)
+- File_followed_by_slash_etc: f.txt/some/more/stuff or f.txt// (not f.txt/, which resolves to a file with [trailing_slash] true); NOTE that f.txt// is sometimes considered acceptable eg on Mac OS X I believe FIXME check
 - Missing_slash_etc: /a/b/missing/some/more/stuff
 
 *)
